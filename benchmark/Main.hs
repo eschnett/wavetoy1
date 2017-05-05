ff4097808eece86a94343e1847b3a94c7883dee2
Erik Schnetter <schnetter@gmail.com>
2017-05-05 12:21:16 -0400

    Reformat code with hindent

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -7,34 +7,47 @@ import WaveToy1
 default (Int)
 
 main :: IO ()
-main = defaultMain $
-  [ bgroup "Grid"
-    [ bench "init" $
-      whnf (\np -> let skel = skeletonGrid (0, 1) np :: Grid Double ()
-                       coords = coordGrid skel
-                       g = initGrid 0 coords
-                   in integralGrid $ energyGrid g) gridSize
-    , bench "rhs" $
-      whnf (\np -> let skel = skeletonGrid (0, 1) np :: Grid Double ()
-                       coords = coordGrid skel
-                       g = initGrid 0 coords
-                       bs = bcGrid g
-                       r = rhsGrid bs g
-                   in integralGrid $ energyGrid r) gridSize
-    , bench "rk2" $
-      whnf (\np -> let skel = skeletonGrid (0, 1) np :: Grid Double ()
-                       coords = coordGrid skel
-                       g = initGrid 0 coords
-                       rhs x = rhsGrid (bcGrid x) x
-                       step = rk2Grid smallStep rhs
-                       g' = iterate step g !! numSteps
-                   in integralGrid $ energyGrid g') gridSize
-    ]
+main =
+  defaultMain $
+  [ bgroup
+      "Grid"
+      [ bench "init" $
+        whnf
+          (\np ->
+             let skel = skeletonGrid (0, 1) np :: Grid Double ()
+                 coords = coordGrid skel
+                 g = initGrid 0 coords
+             in integralGrid $ energyGrid g)
+          gridSize
+      , bench "rhs" $
+        whnf
+          (\np ->
+             let skel = skeletonGrid (0, 1) np :: Grid Double ()
+                 coords = coordGrid skel
+                 g = initGrid 0 coords
+                 bs = bcGrid g
+                 r = rhsGrid bs g
+             in integralGrid $ energyGrid r)
+          gridSize
+      , bench "rk2" $
+        whnf
+          (\np ->
+             let skel = skeletonGrid (0, 1) np :: Grid Double ()
+                 coords = coordGrid skel
+                 g = initGrid 0 coords
+                 rhs x = rhsGrid (bcGrid x) x
+                 step = rk2Grid smallStep rhs
+                 g' = iterate step g !! numSteps
+             in integralGrid $ energyGrid g')
+          gridSize
+      ]
   ]
 
 gridSize :: Int
 gridSize = 1000
+
 numSteps :: Int
 numSteps = 10
+
 smallStep :: Double
 smallStep = 1.0e-8
